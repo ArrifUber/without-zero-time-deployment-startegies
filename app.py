d3d18fa9e0206090b57b614f85a1dfd568ab61ec
ArrifUber <arifbudianto299@gmail.com>
2025-08-13 15:13:06 +0800

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def ping():
 
 @app.route("/")
 def index():
-    return "no zero time deployment (update)", 200
+    return "no zero time deployment (update v.2)", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
