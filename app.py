81f50f6663e68c7c34df55c1dd24e9d82e6f90b1
ArrifUber <arifbudianto299@gmail.com>
2025-08-13 15:16:23 +0800

    update

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 # app.py
-from flask import Flask
+from flask import Flask, request, jsonify
 import time
+from datetime import datetime
 app = Flask(__name__)
 
-@app.route("/ping")
-def ping():
-    return "pong", 200
+APP_VERSION = "1.0.0"
 
 @app.route("/")
-def index():
-    return "no zero time deployment (update v.2)", 200
+def home():
+    return f"Hello from Zero Downtime App! Version: {APP_VERSION}"
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if username == "admin" and password == "123":
+        return jsonify({"message": "Login success", "version": APP_VERSION})
+    return jsonify({"message": "Invalid credentials", "version": APP_VERSION}), 401
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
