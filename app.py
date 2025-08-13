c80319af5c72117031ea9e530c2cfe7901a15f08
ArrifUber <arifbudianto299@gmail.com>
2025-08-13 09:37:14 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask
+import time
+app = Flask(__name__)
+
+@app.route("/ping")
+def ping():
+    return "pong", 200
+
+@app.route("/")
+def index():
+    return "no zero time deployment", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
