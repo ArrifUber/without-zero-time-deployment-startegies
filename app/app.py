03ac93124003e705d6163c2866ef343c4a329ec2
ArrifUber <arifbudianto299@gmail.com>
2025-08-28 08:43:23 +0800

    add /order

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-APP_VERSION = "1.0.4"
+APP_VERSION = "1.1.5"
 
 @app.route("/health")
 def health():
@@ -11,7 +11,7 @@ def health():
 
 @app.route("/")
 def home():
-    return f"Hello from without zero downtime App! Version: {APP_VERSION}"
+    return f"Hello from Without zero dwontime method App! Version: {APP_VERSION}"
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -20,8 +20,17 @@ def login():
     password = data.get("password")
 
     if username == "admin" and password == "123":
-        return jsonify({"message": "Login success", "version": APP_VERSION})
+        return jsonify({"message": "Login success", "version": APP_VERSION, "token": "12345678"})
     return jsonify({"message": "Invalid credentials", "version": APP_VERSION}), 401
+
+@app.route("/order", methods=["POST"])
+def order():
+    data = request.get_json()
+    token = request.headers.get("Authorization")
+    orderData = data.get("order")
+    if token == "12345678":
+        return jsonify({"message": "Succes post order", "order": orderData})
+    return jsonify({"message": "invalid token"}), 401
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
